14eba058b3a72740465a55a7f0e6cd22f50565b8
grantkoji <grantkoji@gmail.com>
2020-05-12 16:26:21 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+
+class Dog
+
+    def initialize(name="Joey", breed="Collie")
+        @name = name
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+end
